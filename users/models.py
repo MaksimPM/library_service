309f5cca9ba98e022c90254526cfab63dacaa9ce
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-12-07 19:00:36 +0300

    Создал модель User, прописал эндпоинты для управления пользователями.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(max_length=255, unique=True, verbose_name='email')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('pk',)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.models import User
+from users.serializers import UserSerializer
+from users.tasks import send_email_task
 
-# Create your views here.
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        send_email_task.delay(user.id)
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
