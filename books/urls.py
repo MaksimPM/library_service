8f1a72918600fd1fa2e90125a1450f73375a4b3d
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-12-07 19:27:59 +0300

    Переименовал name urls для более точного понимания управления

--- books/urls.py
+++ books/urls.py
@@ -7,7 +7,7 @@ app_name = BooksConfig.name
 
 urlpatterns = [
     path('book/create/', BookCreateAPIView.as_view(), name='book_create'),
-    path('books/', BooksListAPIView.as_view(), name='book_list'),
+    path('books/', BooksListAPIView.as_view(), name='books_list'),
     path('book/<int:pk>/', BookRetrieveAPIView.as_view(), name='book_get'),
     path('book/<int:pk>/update/', BookUpdateAPIView.as_view(), name='book_update'),
     path('book/<int:pk>/delete/', BookDestroyAPIView.as_view(), name='book_delete'),
